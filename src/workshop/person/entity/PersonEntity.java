62e86d91485523e53a7f239c28b24832ec00203b
J-millar99 <wlwogus33@gmail.com>
2025-05-05 23:45:06 +0900

    setSsn update

--- src/workshop/person/entity/PersonEntity.java
+++ src/workshop/person/entity/PersonEntity.java
@@ -3,16 +3,64 @@ package workshop.person.entity;
 public class PersonEntity {
 	private String name;
 	private char gender;
+	private String ssn;
 	private String address;
 	private String phone;
 	
+	public PersonEntity(String name, String ssn, String address, String phone) {
+		this.name = name;
+		this.ssn = ssn;
+		this.address = address;
+		this.phone = phone;
+	}
+
 	public PersonEntity() {
 		
 	}
 
-	public PersonEntity(String name, String address, String phone) {
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
 		this.name = name;
+	}
+
+	public char getGender() {
+		return gender;
+	}
+
+	public void setGender(char gender) {
+		this.gender = gender;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
 		this.address = address;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
 		this.phone = phone;
+	}
+
+	public String getSsn() {
+		return ssn;
+	}
+
+	public void setSsn(String ssn) {
+		this.ssn = ssn;
+		char genderNum = ssn.charAt(6);
+		if (genderNum == '1' || genderNum == '3') {
+			setGender('³²');
+		} else if (genderNum == '2' || genderNum == '4') {
+			setGender('¿©');
+		}
 	}
 }
