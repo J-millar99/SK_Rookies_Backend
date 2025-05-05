a9dc66a624e833bc4439f9dd45ccc82b85ad5520
J-millar99 <wlwogus33@gmail.com>
2025-05-06 00:56:42 +0900

    ManageBook Add

--- src/workshop/book/control/ManageBook.java
+++ src/workshop/book/control/ManageBook.java
@@ -1,10 +1,22 @@
 package workshop.book.control;
 
+import workshop.book.entity.Magazine;
+import workshop.book.entity.Novel;
+import workshop.book.entity.Publication;
+
 public class ManageBook {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// Publication 타입 배열을 선언 및 생성
+		Publication[] pubs = new Publication[5];
+		
+		// Magazine 객체 생성
+		// Magazine mz = new Magazine();
+		// Novel 객체 생성
+		// Novel novel = new Novel();
+		
+		// 묶어서 배열에 쓰기 위해서 부모 타입으로 사용
+		Publication pub1 = new Magazine();
+		Publication pub2 = new Novel();
 	}
-
 }
