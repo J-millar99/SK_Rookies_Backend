08d4c8f4c1a91d880ecb6fd733e12ee9c86595ee
J-millar99 <wlwogus33@gmail.com>
2025-05-06 00:44:43 +0900

    Constructor getter setter Add

--- src/workshop/book/entity/Magazine.java
+++ src/workshop/book/entity/Magazine.java
@@ -1,5 +1,22 @@
 package workshop.book.entity;
 
-public class Magazine {
+public class Magazine extends Publication {
+	private String publishingPeriod;
+	
+	public Magazine() {
+		
+	}
 
+	public Magazine(String title, String publishingDate, int page, int price) {
+		super(title, publishingDate, page, price);
+		this.publishingPeriod = publishingPeriod;
+	}
+	
+	public String getPublishingPeriod() {
+		return publishingPeriod;
+	}
+	
+	public void setPublishingPeriod(String publishingPeriod) {
+		this.publishingPeriod = publishingPeriod;
+	}
 }
