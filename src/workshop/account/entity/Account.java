4db41c763cfada7ce93b3f5675fd97ad145bb080
J-millar99 <wlwogus33@gmail.com>
2025-05-05 19:27:49 +0900

    Account method add

--- src/workshop/account/entity/Account.java
+++ src/workshop/account/entity/Account.java
@@ -1,5 +1,51 @@
 package workshop.account.entity;
 
 public class Account {
-
+	private String custId;
+	private String acctId;
+	private int balance;
+	
+	// setter
+	public void setCustId(String custId) {
+		this.custId = custId;
+	}
+	
+	public void setAcctId(String acctId) {
+		this.acctId = acctId;
+	}
+	
+//	public void setBalance(int balance) {
+//		this.balance = balance;
+//	} 
+//	-> 입금 출금으로 대체
+	
+	// getter
+	public String getCustId() {
+		return custId;
+	}
+	
+	public String getAcctId() {
+		return acctId;
+	}
+	
+	public int getBalance() {
+		return balance;
+	}
+	
+	// deposit
+	public void deposit(int amount) {
+		this.balance += amount;
+	}
+	
+	// withdraw
+	public void withdraw(int amount) {
+		if (amount > balance) {
+			System.out.println("잔액 부족");
+		}
+		this.balance -= amount;
+	}
+	
+	public static void main(String[] args) {
+		
+	}
 }
