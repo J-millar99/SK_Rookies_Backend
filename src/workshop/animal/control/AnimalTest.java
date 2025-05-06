6eaf5015a9b8ddb32ad012fcc0fe8f609857a081
J-millar99 <wlwogus33@gmail.com>
2025-05-06 16:06:07 +0900

    AnimalTest update

--- src/workshop/animal/control/AnimalTest.java
+++ src/workshop/animal/control/AnimalTest.java
@@ -11,6 +11,16 @@ public class AnimalTest {
 		Cat cat1 = new Cat();
 		Pet cat2 = new Cat();
 		Animal cat3 = new Cat();
+		
+		// Pet으로부터 상속 받은 메서드 
+		cat1.setName("톰");
+		System.out.println(cat1.getName());
+		cat1.play();
+		// Animal로부터 상속받은 메서드
+		cat1.eat();
+		cat1.walk();
+		
+		
 	}
 
 }
